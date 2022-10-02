bae6317a228facf7ceb02f82824e0aeaa5e31020
“Krystian” <scoverstudio@gmail.com>
2022-10-03 00:13:55 +0200

    add pdf downloader

--- src/components/views/Resume/Resume.js
+++ src/components/views/Resume/Resume.js
@@ -1,14 +1,36 @@
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import styles from "./Resume.module.scss";
 
 const Resume = () => {
   return (
     <section className={styles.resumeContainer}>
-      <Button>Download CV</Button>
+      <Button>
+        <Link
+          to={`${process.env.PUBLIC_URL}/files/Minimalist-CV-Resume.pdf`}
+          target="_blank"
+          download
+          className={styles.cv}
+        >
+          Download CV
+        </Link>
+      </Button>
       <div className={styles.resumeImg}>
-        <img alt="" src={`${process.env.PUBLIC_URL}/images/myRESUME.png`}></img>
+        <img
+          alt=""
+          src={`${process.env.PUBLIC_URL}/images/myRESUMECV.png`}
+        ></img>
       </div>
-      <Button>Download CV</Button>
+      <Button>
+        <Link
+          to={`${process.env.PUBLIC_URL}/files/Minimalist-CV-Resume.pdf`}
+          target="_blank"
+          download
+          className={styles.cv}
+        >
+          Download CV
+        </Link>
+      </Button>
     </section>
   );
 };
